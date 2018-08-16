899108593ad87f12cbc1df303c357045cd53f9d6
marjjjanna <mariapszonka999@gmail.com>
2018-08-16 19:11:46 +0200

    First commit

---
+++ timer.js
@@ -0,0 +1,10 @@
+PS C:\code> echo "# small_JS_projects" >> README.md
+PS C:\code> git init
+Initialized empty Git repository in C:/code/.git/
+PS C:\code> git add README.md
+PS C:\code> git commit -m "first commit"
+[master (root-commit) 75e0780] first commit
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 README.md
+PS C:\code> git remote add origin https://github.com/marjjjanna/small_JS_projects.git
+PS C:\code> git push -u origin master
