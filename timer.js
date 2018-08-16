5de109218454f21c7ea32d817b9c2c7820224cdc
marjjjanna <mariapszonka999@gmail.com>
2018-08-16 20:51:04 +0200

    ech!

--- timer.js
+++ timer.js
@@ -1,10 +1,11 @@
-PS C:\code> echo "# small_JS_projects" >> README.md
-PS C:\code> git init
-Initialized empty Git repository in C:/code/.git/
-PS C:\code> git add README.md
-PS C:\code> git commit -m "first commit"
-[master (root-commit) 75e0780] first commit
- 1 file changed, 0 insertions(+), 0 deletions(-)
- create mode 100644 README.md
-PS C:\code> git remote add origin https://github.com/marjjjanna/small_JS_projects.git
-PS C:\code> git push -u origin master
+
+
+function countTimeDown(minutes = 0, seconds = 0, miliseconds = 0) {
+    minutes++;
+    seconds++;
+    miliseconds++;
+    
+    var time = minutes + ":" + seconds + ":" + milliseconds;
+    document.getElementById("1234").innerHTML = time;
+    setTimeout("countTimeDown(minutes, seconds, miliseconds)", 10);
+}
