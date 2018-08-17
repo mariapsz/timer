82a8ff9a5e7727eb6ad7c3137e6d86177cc156a3
marjjjanna <mariapszonka999@gmail.com>
2018-08-17 02:44:02 +0200

    troche dziala

--- timer.js
+++ timer.js
@@ -1,11 +1,35 @@
 
 
-function countTimeDown(minutes = 0, seconds = 0, miliseconds = 0) {
-    minutes++;
-    seconds++;
-    miliseconds++;
+function countTimeDown(deciseconds = 0) {
     
-    var time = minutes + ":" + seconds + ":" + milliseconds;
-    document.getElementById("1234").innerHTML = time;
-    setTimeout("countTimeDown(minutes, seconds, miliseconds)", 10);
+    if(document.getElementById("ifShouldWorks").innerText.includes('true')){
+    var ds = deciseconds;
+    ds++;
+    document.getElementById("timer").innerHTML = createNotation(deciseconds);
+
+    setTimeout(function(){countTimeDown(ds)}, 100);
+    }
 }
+ function createNotation(deciseconds) {
+    var sek = Math.floor(deciseconds/10);
+    deciseconds %= 10;
+    var min = 0;
+
+    if(sek >= 60) {
+        min = Math.floor(sek/60);
+        sek %= 60;
+    } 
+    return min + ':' + sek + ':' + deciseconds;
+ }
+
+ function stop() {
+    var temp = document.getElementById('timer').innerText;
+    document.getElementById("ifShouldWorks").innerText = 'false';
+    console.log('stop');
+}
+
+ function reset() {
+    document.getElementById('timer').innerHTML = '00:00:00';
+    console.log('reset');
+ }
+
