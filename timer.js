d717be440b8399bcd47734a3dcf3ba85f1104098
marjjjanna <mariapszonka999@gmail.com>
2018-08-18 13:13:26 +0200

    Final final version

--- timer.js
+++ timer.js
@@ -1,30 +1,30 @@
-var shouldWork = true;
+var shouldWork = false;
 var ds = 0;
 var s = 0;
 var min = 0;
 
 function countTimeDown() {
     
-    if(shouldWork){
-    ds++;
-    document.getElementById("timer").innerText = createNotation();
-
-    setTimeout(function(){countTimeDown(ds)}, 100);
+    if (shouldWork) {
+        ds++;
+        document.getElementById("timer").innerText = createNotation();
+        setTimeout(function(){countTimeDown(ds)}, 100);
     }
 }
- function createNotation() {
-    if (ds == 10){
+
+function createNotation() {
+    if (ds == 10) {
         s++;
         ds = 0;
     }
-    if (s == 60){
+    if (s == 60) {
         min++;
         s = 0;
     }
-    if (min == 59)
-    {
+    if (min == 59) {
         shouldWork = false;
     }
+    //add '0' before if number < 10
     if (min < 10) {
         if (s < 10){
             if (ds < 10)
@@ -32,29 +32,35 @@ function countTimeDown() {
             
             return '0' + min + ':' + '0' + s + ':' + ds;
         }
+        if (ds < 10)
+            return '0' + min + ':' + s + ':' + '0' + ds;
+            
         return '0' + min + ':' + s + ':' + ds;
     }
     return min + ':' + s + ':' + ds;
  }
 
- function stop() {
-    shouldWork = false;
- }
- function start() {
-    shouldWork = true;
-    countTimeDown();
- }
+function stop() {
+   shouldWork = false;
+}
 
- function reset() {
+function start() {
+   if (shouldWork == false) {
+       shouldWork = true;
+       countTimeDown();
+    }
+}
+
+function reset() {
     shouldWork = false;
     min = 0;
     s = 0;
     ds = 0;
     document.getElementById('timer').innerHTML = createNotation();
     document.getElementById('placeForSplittedTime').innerText = '';
- }
+}
 
- function split() {
+function split() {
     var currentTime = document.getElementById('timer').innerText;
     document.getElementById('placeForSplittedTime').innerText += '\n\r'+ currentTime;
- }
+}
